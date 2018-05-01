740c73efe4eeb73904c208e4e293860dd6395e38
Drew Wilken <wilk0001@stthomas.edu>
2018-04-30 22:36:08 -0500

    Initial Commit

---
+++ Ice Lang/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Ice Lang
+//
+//  Created by Drew Wilken on 4/30/18.
+//  Copyright © 2018 Drew Wilken. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
